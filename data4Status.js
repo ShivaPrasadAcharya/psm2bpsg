fe6476db0eb9d9ddb86000e01d6b0e13555edc9e
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-11-18 17:53:01 +0000

    ok

--- data4Status.js
+++ data4Status.js
@@ -1,13 +1,36 @@
-var data4Status = `SNo,Name,Topic1,Topic2,Status/Remarks
-
+var data4Status = `SNo,Name(RollNo),Topic,Remarks(✗=not submitted, ✓=submitted)
+01,Aaryaka Timsina(16),e-Identity (Estonia),✗
+02,Aashma pokharel(2),e-Residency (Estonia),✗
+03,Aastha Yadav(8),Smart-ID (Estonia),✗
+04,Anjali Chaudhary(5),X-Road (Estonia),✗
+05,Asmita Rai(4),e-Land Register (Estonia),✗
+06,Asmita Upreti(14),Population Registry (Estonia),✗
+07,Deeksha Rani(18),E-waste Recycling (China),✗
+08,Depa oli(15),KSI Blockchain (Estonia),✗
+09,Dibina Dangol(6),e-Justice (Estonia),✗
+10,Kritika Kathyat(1),e-Police (Estonia),✗
+11,Mallika Dwariya(27),i-Voting (Estonia),✗
+12,Manisha Yadav(20),Government Cloud (Estonia),✗
+13,Merika Mote(26),Intelligent Transportation Systems (Estonia),✗
+14,Nutan Thakur(9),Data Embassy (Estonia),✗
+15,Parista Desar(10),Artificial Intelligence and Regulatory Sandbox (Singapore),✗
+16,Pratistha Khanal(23),Dubai 10x Innovations (UAE),✗
+17,Pritee Kumari Pandit(19),Global Development Lab (USA),✗
+18,Puspa Upadhayay(25),Seoul 50+ Policy (South Korea),✗
+19,Samriddhi Bhandari(12),Patient Innovation Platform(Portugal),✗
+20,Sapana Kumara Jha(17),Hyperloop Transportation (UAE),✗
+21,Shiksha Sharrof(22),M-Pesa Mobile Money (Kenya),✗
+22,Sunita Pariyar(7),Smart Nation Initiative (Singapore),✗
+23,Suruchi Panday(21),E-Government Development Index (UN),✗
+24,Deepika yadav(3),Digital Public Infrastructure (India),✗
 
 `;
 
 
 // Dataset metadata
 var data4StatusInfo = {
-    name: "Assignments-Comming Soon",
-    description: "Assignments Topics & Status ",
+    name: "Assignments",
+    description: "Powerpoint Submission Deadline-Mansir29",
     emoji: "📝",
     columns: 6,
     primaryKey: "SNo",
