be3cd70c1f864575fad73217f76dbaa5edaed4b1
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-11-18 19:40:20 +0545

    Update data9Videos1.js

--- data9Videos1.js
+++ data9Videos1.js
@@ -1,7 +1,7 @@
 // data9video1.js - Videos Dataset
 var data9Videos1 = `ID,Title,Description,shorturl,longurl
 001,Motivation for Exam,Premanadaji,see video,https://www.youtube.com/shorts/Jr4_hHwVfJw
-002,Religion+,PodCast,Shankaracharya Nischalananda,https://www.youtube.com/watch?v=G6k8IQ_sYpk&t=186s
+
 `;
 
 // Dataset metadata
@@ -15,3 +15,4 @@ var data9Videos1Info = {
         "001": "green",
     }
 };
+
