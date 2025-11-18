bf3748c02d3f3445c4278a6a30627794c5ef14ea
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-11-18 19:22:05 +0545

    Update data3Links.js

--- data3Links.js
+++ data3Links.js
@@ -8,15 +8,18 @@ var data3Links = `ID,Categry,Subject,Remark,Link
 007a,Answers Sheet-PSMII,Topper Paper-Ist schedule exam 2081 ,pdf,DOCS/bestanswerpsm2081.pdf
 007b,Best Answers-PSMII,Jeeya's Answer Sheet-PreBoard Exam-2082,pdf,DOCS/bestanswerofpreboard2082.pdf
 007c,Answers-PSMII,Dikshya Answer Sheet-2082 IInd schedule exam,pdf,DOCS/secondtestanswersheet2082.pdf
-009,Notes,Global Development Lab of USA,html,HTML/GDL-USA.htm
-010,Notes,patient innovation platform of portugal,html,HTML/patientinnovation.htm
-011,PPT by Dikshya,population registry of estonia,pdf,DOCS/populationregistryestonia.pdf
-012,PPT by Jeeya,government cloud of estonia,pdf,DOCS/govcloudbyjeeya.pdf
-013,PPT by Divya,E-Waste recycling of China,pdf,DOCS/ewasterecyclingofchinabydivya.pdf
-014,PPT by Archana,X-Road of Estonia,pdf,DOCS/XroadEstoniabyarchana.pdf
-015,PPT by Pratikshya,Intelligent Transport System,pdf,DOCS/intelligenttransportbypratikshya.pdf
-016,PPT by Jamuna,e-police,pdf,DOCS/epolicebyjamuna.pdf
+
 `;
+//push them to main in appropriate time
+//009,Notes,Global Development Lab of USA,html,HTML/GDL-USA.htm
+//010,Notes,patient innovation platform of portugal,html,HTML/patientinnovation.htm
+//011,PPT by Dikshya,population registry of estonia,pdf,DOCS/populationregistryestonia.pdf
+//012,PPT by Jeeya,government cloud of estonia,pdf,DOCS/govcloudbyjeeya.pdf
+//013,PPT by Divya,E-Waste recycling of China,pdf,DOCS/ewasterecyclingofchinabydivya.pdf
+//014,PPT by Archana,X-Road of Estonia,pdf,DOCS/XroadEstoniabyarchana.pdf
+//015,PPT by Pratikshya,Intelligent Transport System,pdf,DOCS/intelligenttransportbypratikshya.pdf
+//016,PPT by Jamuna,e-police,pdf,DOCS/epolicebyjamuna.pdf
+
 // Dataset metadata
 var data3LinksInfo = {
     name: "Notes",
@@ -25,6 +28,7 @@ var data3LinksInfo = {
     columns: 5,
     primaryKey: "ID"
 };
+
 
 
 
