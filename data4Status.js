f5a027e471adaa915b5490879a06e6191acc604e
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-11-18 19:37:17 +0545

    Update data4Status.js

--- data4Status.js
+++ data4Status.js
@@ -1,22 +1,5 @@
 var data4Status = `SNo,Name,Topic1,Topic2,Status/Remarks
-1,Aarya Shree Neupane,e-Identity (Estonia),Metaverse Governance,✓
-2,Anju Kumari Mahato,e-Residency (Estonia),Green Hydrogen Technology,✓
-3,Anu Yaduwanshi,Smart-ID (Estonia),Brain-Computer Interfaces,✓ (only topic1 presented)
-4,Archana Karki,X-Road (Estonia),Autonomous Delivery Systems,✓
-5,Chetana Bhandari,e-Land Register (Estonia),Synthetic Biology,✓
-6,Dikshya Bhattarai,Population Registry (Estonia),Space-Based Solar Power,✓(Best Presentator)
-7,Divya Shree Acharya,E-waste Recycling (China),Swarm Robotics,✓
-8,Ganga Acharya,KSI Blockchain (Estonia),Carbon Capture and Storage,✓
-9,Gazal Bhattarai,e-Justice (Estonia),Augmented Reality in Healthcare,✓
-10,Jamuna Acharya,e-Police (Estonia),Decentralized Finance (DeFi),✓
-11,Jeeya Shakya,i-Voting (Estonia),Biometric Authentication Systems,✓(Logical Answer Writer)
-12,Mausami Shahi,Government Cloud (Estonia),Urban Vertical Farming,✓
-13,Pratikshya Bastola,Intelligent Transportation Systems (Estonia),Neuromorphic Computing,✓(Best Role Player)
-14,Rima Khadka,Data Embassy (Estonia),Smart Grid Modernization,✗
-15,Sakshi Kumari Jha,Artificial Intelligence and Regulatory Sandbox (Singapore),Gene Editing (CRISPR),✓
-16,Supnima KC,Dubai 10x Innovations (UAE),Patient Innovation Platform of Portugal,✓
-17,Sweta Ray,Global Development Lab (USA),Hyperloop Transportation,✓
-18,Yunika Guragain,Seoul 50+ Policy (South Korea),Ocean Energy Harvesting,✓(Unique Answer Writer)
+
 
 `;
 
@@ -31,4 +14,5 @@ var data4StatusInfo = {
     rowColors: {
                "14": "red"
     }
+
 };
