cfcc2bf770f20f4205e36e133ba17f36a24d623f
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-11-18 22:19:05 +0545

    Update data8html2.js

--- data8html2.js
+++ data8html2.js
@@ -1,20 +1,14 @@
 // data8html2.js
 window.html2Content = `
-<h1 id="civil-law-i-general-principles-and-provisions-of-contract-">Guru Vani</h1>
-<h3> Don't just collect Zeros.Zero plus Zero is Zero. Zero multiplying Zero is Zero. Add some value before these collected zeros. Enjoy every moment to the fullest.</h3>
-<h4> You are not born just to have a academic degrees. There is much higher purpose of life. Make an Inquiry into it & Demystify it.</h4>
-<p> Life is preparation & Death is Examination. Better be Fail in your academic papers but never get fail in that Final Exam.</p>
-<p> To Get+To Give = Pain<br>
-ForGet + ForGive = Pleasure <br>
-So do not seek love and attention from other & do not expose your love and attention to others.</p>
-<p> एकबार झुक कर सम्बन्ध सुधार जाये तो झुक जाओ परन्तु बार बार आपको नै झुक्न परे तो रूक जाओ। </p>
+
 
 `;
 
 window.html2Meta = { 
-  name: "गुरूवाणि",
+  name: "no content yet",
    emoji: "📝" 
  };
+
 
 
 
