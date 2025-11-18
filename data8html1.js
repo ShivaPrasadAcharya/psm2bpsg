bd2a5946a78546ede7d029ec2f96bf0a2f42777d
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-11-18 22:18:09 +0545

    Update data8html1.js

--- data8html1.js
+++ data8html1.js
@@ -1,11 +1,11 @@
 // data8html1.js
 window.html1Content = `
-<h1 id="civil-law-i-general-principles-and-provisions-of-contract-">Best of Luck for your Board Exam.</h1>
-<p> You are your own competitor. Be Better than Before.</p>
+
 `;
 window.html1Meta = { 
-  name: "no content",
+  name: "no content yet",
    emoji: "📝" 
 
   };
+
 
