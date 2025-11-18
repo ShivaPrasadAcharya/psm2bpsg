b0cb9fcb978ef4ba32ccf83fdab414b531d438da
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-11-18 22:13:10 +0545

    Update data1Trainees.js

--- data1Trainees.js
+++ data1Trainees.js
@@ -1,29 +1,29 @@
 // data1Trainees.js - Employees Dataset (Additional Data)
-var data1Trainees = `RollNo.,Name,Per. Address,Temp.Address,Email,Phone No.
-1,kritika kathyat,Doti,Naval,kritikakathayat16@gmail.com,9848406622
-2,Aashma pokharel,Syangja,Kritipur,aashmapokhrel831@gmail.com,9861882828
-3,Deepika yadav,Siraha,Bagbazar,deepikayadav1460@gmail.com,9814087947
-4,Asmita Rai,Okhaldhunga,Boudha,asmitarumdali612@gmail.com,9863854010
-5,Anjali Chaudhary,Mahottari,Chovar,anjalichaudhary5224@gmail.com,9812092333
-6,Dibina Dangol,Patan,Shankhamul,dangoldibina8@gmail.com,9808103666
-7,Sunita Pariyar,Khotang,Sinamangal,sunitapariyard6@gmail.com,9847372387
-8,Aastha Yadav,Siraha,Bhaktapur,ayadavbs10@gmail.com,9863978285
-9,Nutan Thakur,Siraha,Imadol,nutant177@gmail.com,9818022467
-10,Parista Desar,Bungmati,Bungmati,paristadesar2059@gmail.com,9860684100
-12,Samriddhi Bhandari,Kapilvastu,Budhanilkantha,samriddhi998@gmail.com,9807496921
-14,Asmita Upreti,Bara,Pescicola,asmitauprety939@gmail.com,9865369597
-15,Dipa oli,Salyan,Bagbazar,dipaoli097@gmail.com,9742237758
-16,Aaryaka Timsina,Bhaktapur,Bhaktapur,timsina.aarya@gmail.com,9862366373
-17,Sapana kumara Jha,Rautahat,Sanagau,jhasap1456@gmail.com,9763693082
-18,Deeksha Rani,Dhanusha,Sinnamangal,ranid7098@gmail.com,9829918264
-19,Pritee Kumar Pandit,Sarlahi,Tikathali,Panditpritee855@gmail.com,9829999023
-20,Manisha Yadav,Bara,Bagbazar,susiladeviyadav5@gmail.com,9823673059
-21,Suruchi Panday,Arghakhanchi,Naikap,suruchi.p02@gmail.com,9749785323
-22,Shiksha Sharrof,Sarlahi,Tinkune,sharrofshiksha@gmail.com,9748449400
-23,Pratistha Khanal,Gulmi,Naikap,pratisthakhanal99@gmail.com,9847487624
-25,Puspa Upadhayay,Jumla,Kapan,upadhayapuspa1@gmail.com,9863197791
-26,Merika Mote,Udayapur,Tinkune,merikamote@gmail.com,9749231291
-27,Mallika Dwariya,Doti,New Plaza,mallikadwariya@gmail.com,9862477612`;
+var data1Trainees = `SNo,RollNo.,Name,Per. Address,Temp.Address,Email,Phone No.
+01,1,Kritika Kathyat,Doti,Naxal,kritikakathayat16@gmail.com,9848406622
+02,2,Aashma pokharel,Syangja,Kritipur,aashmapokhrel831@gmail.com,9861882828
+03,3,Deepika yadav,Siraha,Bagbazar,deepikayadav1460@gmail.com,9814087947
+04,4,Asmita Rai,Okhaldhunga,Boudha,asmitarumdali612@gmail.com,9863854010
+05,5,Anjali Chaudhary,Mahottari,Chovar,anjalichaudhary5224@gmail.com,9812092333
+06,6,Dibina Dangol,Patan,Shankhamul,dangoldibina8@gmail.com,9808103666
+07,7,Sunita Pariyar,Khotang,Sinamangal,sunitapariyard6@gmail.com,9847372387
+08,8,Aastha Yadav,Siraha,Bhaktapur,ayadavbs10@gmail.com,9863978285
+09,9,Nutan Thakur,Siraha,Imadol,nutant177@gmail.com,9818022467
+10,10,Parista Desar,Bungmati,Bungmati,paristadesar2059@gmail.com,9860684100
+11,12,Samriddhi Bhandari,Kapilvastu,Budhanilkantha,samriddhi9988@gmail.com,9807496921
+12,14,Asmita Upreti,Bara,Pescicola,asmitauprety939@gmail.com,9865369597
+13,15,Dipa oli,Salyan,Bagbazar,dipaoli097@gmail.com,9742237758
+14,16,Aaryaka Timsina,Bhaktapur,Bhaktapur,timsina.aarya@gmail.com,9862366373
+15,17,Sapana kumara Jha,Rautahat,Sanagau,Jhasap1456@gmail.com,9763693082
+16,18,Deeksha Rani,Dhanusha,Sinnamangal,ranid7098@gmail.com,9829918264
+17,19,Pritee Kumari Pandit,Sarlahi,Tikathali,Panditpritee855@gmail.com,9829999023
+18,20,Manisha Yadav,Bara,Bagbazar,susiladeviyadav5@gmail.com,9823673059
+19,21,Suruchi Panday,Arghakhanchi,Naikap,suruchi.p02@gmail.com,9749785323
+20,22,Shiksha Sharrof,Sarlahi,Tinkune,sharrofshiksha@gmail.com,9748449400
+21,23,Pratistha Khanal,Gulmi,Naikap,pratisthakhanal99@gmail.com,9847487624
+22,25,Puspa Upadhayay,Jumla,Kapan,upadhayapuspa1@gmail.com,9863197791
+23,26,Merika Mote,Udayapur,Tinkune,merikamote@gmail.com,9749231291
+24,27,Mallika Dwariya,Doti,New Plaza,mallikadwariya@gmail.com,9862477612`;
 // Dataset metadata
 var data1TraineesInfo = {
     name: "Students Record",
@@ -35,5 +35,6 @@ var data1TraineesInfo = {
         "01": "green",
     }
 };
+
 
 
