1934a3277ac3596e139151fc47617bbd270aa052
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-11-24 16:49:08 +0000

    okk

--- data5Url2.js
+++ data5Url2.js
@@ -2,12 +2,12 @@ var data5Url2 = `sno,subject,description,shorturl,longurl
 01,Contact List,BPSG 7th sem-2082(Mansir),open word doc,DOCS/studentinfo.docx
 02,Contact List,BPSG 7th sem-2082(Mansir),open pdf file,DOCS/studentinfo.pdf
 03, LAW, land related act-2021, open doc file, DOCS/landrelatedact2021.docx
-03, LAW, civil code-2074, open doc file, DOCS/civilcode2074.docx
+04, LAW, civil code-2074, open doc file, DOCS/civilcode2074.docx
 `;
 
 // Dataset metadata
 var data5Url2Info = {
-    name: "Contact",
+    name: "Laws",
     description: "Important Urls-non git links, color columns",
     emoji: "🔗",
     columns: 5,
