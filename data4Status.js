355a52f9517a08aa634437d979a6bd3f4bd3c188
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-12-16 12:46:38 +0000

    ok

--- data4Status.js
+++ data4Status.js
@@ -1,16 +1,16 @@
 var data4Status = `SNo,Name(RollNo),Topic,Remarks(✗=not submitted/ ✓=submitted)
 01,Aaryaka Timsina(16),e-Identity (Estonia),✓
-02,Aashma Pokharel(2),e-Residency (Estonia),✗
+02,Aashma Pokharel(2),e-Residency (Estonia),✓
 03,Aastha Yadav(8),Smart-ID (Estonia),✓
 04,Anjali Chaudhary(5),X-Road (Estonia),✓
 05,Asmita Rai(4),e-Land Register (Estonia),✓
 06,Asmita Upreti(14),Population Registry (Estonia),✓
-07,Deeksha Rani(18),E-waste Recycling (China),✗
-08,Depa Oli(15),KSI Blockchain (Estonia),✗
+07,Deeksha Rani(18),E-waste Recycling (China),✓
+08,Depa Oli(15),KSI Blockchain (Estonia),✓
 09,Dibina Dangol(6),e-Justice (Estonia),✓
-10,Kritika Kathyat(1),e-Police (Estonia),✗
+10,Kritika Kathyat(1),e-Police (Estonia),✓
 11,Mallika Dwariya(27),i-Voting (Estonia),✓
-12,Manisha Yadav(20),Government Cloud (Estonia),✗
+12,Manisha Yadav(20),Government Cloud (Estonia),✓
 13,Merika Mote(26),Intelligent Transportation Systems (Estonia),✓
 14,Nutan Thakur(9),Data Embassy (Estonia),✓
 15,Parista Desar(10),Artificial Intelligence and Regulatory Sandbox (Singapore),✗
@@ -18,7 +18,7 @@ var data4Status = `SNo,Name(RollNo),Topic,Remarks(✗=not submitted/ ✓=submitt
 17,Pritee Kumari Pandit(19),Global Development Lab (USA),✓
 18,Puspa Upadhayay(25),Seoul 50+ Policy (South Korea),✗
 19,Samriddhi Bhandari(12),Patient Innovation Platform(Portugal),✓
-20,Sapana Kumara Jha(17),Hyperloop Transportation (UAE),✗
+20,Sapana Kumara Jha(17),Hyperloop Transportation (UAE),✓
 21,Shiksha Sharrof(22),M-Pesa Mobile Money (Kenya),✗
 22,Sunita Pariyar(7),Smart Nation Initiative (Singapore),✓
 23,Suruchi Panday(21),E-Government Development Index (UN),✓
