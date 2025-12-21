dae776708b0fae84122cb28cc8d092179d6f935f
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-12-21 16:15:49 +0000

    ok

--- data5Url2.js
+++ data5Url2.js
@@ -3,6 +3,8 @@ var data5Url2 = `sno,subject,description,shorturl,longurl
 02,Contact List,BPSG 7th sem-2082(Mansir),open pdf file,DOCS/studentinfo.pdf
 03, LAW, land related act-2021, open doc file, DOCS/landrelatedact2021.docx
 04, LAW, civil code-2074, open doc file, DOCS/civilcode2074.docx
+05, LAW, PPP Act, open pdf file, DOCS/PPP Act 2075.pdf
+05, LAW, PPP Policy, open pdf file, DOCS/PPP Policy 2072.pdf
 `;
 
 // Dataset metadata
