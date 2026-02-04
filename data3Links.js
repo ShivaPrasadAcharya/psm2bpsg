1f74a064331b0df9a1dfdb6d1f6108e13526f92c
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2026-02-04 04:24:39 +0000

    ppt in pdf added

--- data3Links.js
+++ data3Links.js
@@ -9,17 +9,40 @@ var data3Links = `ID,Categry,Subject,Remark,Link
 007a,Answers Sheet-PSMII,Topper Paper-Ist schedule exam 2081 ,pdf,DOCS/bestanswerpsm2081.pdf
 007b,Best Answers-PSMII,Jeeya's Answer Sheet-PreBoard Exam-2082,pdf,DOCS/bestanswerofpreboard2082.pdf
 007c,Answers-PSMII,Dikshya Answer Sheet-2082 IInd schedule exam,pdf,DOCS/secondtestanswersheet2082.pdf
+009,Notes,Global Development Lab of USA,html,HTML/GDL-USA.htm
+010,Notes,patient innovation platform of portugal,html,HTML/patientinnovation.htm
+011,PPT by Dikshya,population registry of estonia,pdf,DOCS/populationregistryestonia.pdf
+012,PPT by Jeeya,government cloud of estonia,pdf,DOCS/govcloudbyjeeya.pdf
+013,PPT by Divya,E-Waste recycling of China,pdf,DOCS/ewasterecyclingofchinabydivya.pdf
+014,PPT by Archana,X-Road of Estonia,pdf,DOCS/XroadEstoniabyarchana.pdf
+015,PPT by Pratikshya,Intelligent Transport System,pdf,DOCS/intelligenttransportbypratikshya.pdf
+016,PPT by Jamuna,e-police,pdf,DOCS/epolicebyjamuna.pdf
+017,PDF,PPP Act 2075,pdf,DOCS/PPP Act 2075.pdf
+018,PDF,PPP Policy 2072,pdf,DOCS/PPP Policy 2072.pdf
+019,Presentation,General Presentation,powerpoint,DOCS/Presentation.pptx
+020,Presentation,General Presentation,pdf,DOCS/Presentation.pdf
+021,Syllables,PSMII Syllables,word doc,DOCS/psm2syllablesbpsg.docx
+022,PDF,a_2,pdf,DOCS/a_2.pdf
+023,PDF,Dibina Dangol_06,pdf,DOCS/Dibina Dangol_06.pdf
+024,PDF,Digital Public Infrastructure (India),pdf,DOCS/Digital Public Infrastructure (India).pdf
+025,PDF,DOC-20251215-WA0000.(4),pdf,DOCS/DOC-20251215-WA0000.(4).pdf
+026,Presentation,E-Government Development Index (UN),powerpoint,DOCS/E-Government Development Index (UN).pptx
+027,PDF,E-Residency Estonia,pdf,DOCS/E-Residency(Estonia).pdf
+028,PDF,e-police (1),pdf,DOCS/epolice (1).pdf
+029,PDF,Global Development Lab (USA) 2,pdf,DOCS/Global Development Lab (USA) 2.pdf
+030,PDF,Government Cloud Estonia,pdf,DOCS/govcloudbyjeeva.pdf
+031,Presentation,Government Cloud Estonia,powerpoint,DOCS/GOVERNMENT CLOUD ESTONIA.pptx
+032,PDF,Hyperloop Transportation,pdf,DOCS/Hyperloop Transportation.pdf
+033,PDF,KSI Blockchain Estonia,pdf,DOCS/KSI Blockchain Estonia final one.pdf
+034,PDF,Smart Nation Singapore Summary,pdf,DOCS/Smart_Nation_Singapore_Summary.pdf
+035,Document,Civil Code 2074,word doc,DOCS/civilcode2074.docx
+036,Document,Land Related Act 2021,word doc,DOCS/landrelatedact2021.docx
+037,PDF, e-identy by aaryaka, pdf doc, DOCS/e-identy by aaryaka.pdf
+038,PDF, i-voting, pdf doc, DOCS/i-voting (Estonia).pdf
+
 
 `;
 //push them to main in appropriate time
-//009,Notes,Global Development Lab of USA,html,HTML/GDL-USA.htm
-//010,Notes,patient innovation platform of portugal,html,HTML/patientinnovation.htm
-//011,PPT by Dikshya,population registry of estonia,pdf,DOCS/populationregistryestonia.pdf
-//012,PPT by Jeeya,government cloud of estonia,pdf,DOCS/govcloudbyjeeya.pdf
-//013,PPT by Divya,E-Waste recycling of China,pdf,DOCS/ewasterecyclingofchinabydivya.pdf
-//014,PPT by Archana,X-Road of Estonia,pdf,DOCS/XroadEstoniabyarchana.pdf
-//015,PPT by Pratikshya,Intelligent Transport System,pdf,DOCS/intelligenttransportbypratikshya.pdf
-//016,PPT by Jamuna,e-police,pdf,DOCS/epolicebyjamuna.pdf
 
 // Dataset metadata
 var data3LinksInfo = {
