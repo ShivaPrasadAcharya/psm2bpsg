8a7c9c411dfd86f4e8813fac618d2a3071507a37
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2026-03-26 06:28:15 +0000

    ok

--- data3Links.js
+++ data3Links.js
@@ -1,21 +1,18 @@
 // data3Links.js - Links Dataset
 var data3Links = `ID,Categry,Subject,Remark,Link
-00,BestAnswer-LongQuestionOnly-AaryakanAashma,2082 Chaitra-PreBoard,pdf,DOCS/BestAnswerPreboard2082ChaitraAashmaAaryaka.pdf
+00,BestAnswer-LongQuestionOnly-Aaryaka_Aashma,2082 Chaitra-PreBoard,pdf,DOCS/BestAnswerPreboard2082ChaitraAashmaAaryaka.pdf
 000,BestAnswer-Aastha-Aaryaka-Sunita,2082 Magh-IInd Schedule,pdf,DOCS/BestAnswerofIIndScheduledExam208maghbyAasthaAaryakaSunita.pdf
-001,Make a Call-2079batch,BPSG 7th sem-2082(Mansir),Contact Directory,https://shivaprasadacharya.github.io/callbpsgforthbatch/
-002,1.Contacts List,BPSG 7th sem-2082(Mansir),pdf,DOCS/studentinfo.pdf
-003,2.Syllables,BPSG 7th sem-PSMII syllables,html,HTML/psm2syllablesbpsg.htm
-004,3.Past Questions,BPSG 7th sem-PSMII Past Question,pdf,DOCS/pastquestions.pdf
-005,4.Past Questions,BPSG 7th sem-PSMII Past Question,html,HTML/pastquestions.htm
-006,5.Contacts,BPSG 7th sem-2082 contact list,word doc,DOCS/studentinfo.docx
-007,6.Answers Sheet-PSMII,Topper Paper-Ist schedule exam 2081 ,pdf,DOCS/bestanswerpsm2081.pdf
-008,7.Best Answers-PSMII,Jeeya's Answer Sheet-PreBoard Exam-2082,pdf,DOCS/bestanswerofpreboard2082.pdf
-009,8.Answers-PSMII,Dikshya Answer Sheet-2082 IInd schedule exam,pdf,DOCS/secondtestanswersheet2082.pdf
+008,Best Answers-PSMII,Jeeya's Answer Sheet-PreBoard Exam-2082,pdf,DOCS/bestanswerofpreboard2082.pdf
+007,Answers Sheet-PSMII,Topper Paper-Ist schedule exam 2081 ,pdf,DOCS/bestanswerpsm2081.pdf
+009,Answers-PSMII,Dikshya Answer Sheet-2082 IInd schedule exam,pdf,DOCS/secondtestanswersheet2082.pdf
+004,Past Questions,BPSG 7th sem-PSMII Past Question,pdf,DOCS/pastquestions.pdf
+014,Syllables,PSMII Syllables,word doc,DOCS/psm2syllablesbpsg.pdf
+014,Syllables,PSMII Syllables,word doc,DOCS/psm2syllablesbpsg.docx
+002,Contacts List,BPSG 7th sem-2082(Mansir),pdf,DOCS/studentinfo.pdf
 010,Notes,Global Development Lab of USA,html,HTML/GDL-USA.htm
 011,Notes,patient innovation platform of portugal,html,HTML/patientinnovation.htm
 012,Presentation,General Presentation,powerpoint,DOCS/Presentation.pptx
 013,Presentation,General Presentation,pdf,DOCS/Presentation.pdf
-014,Syllables,PSMII Syllables,word doc,DOCS/psm2syllablesbpsg.docx
 015,Nutan Thakur-2079 batch,Data Embassay,pdf,DOCS/a_2.pdf
 016,Dibina Dangol-2079 batch,E-Justice,pdf,DOCS/Dibina Dangol_06.pdf
 017,Deepika Yadav,Digital Public Infrastructure (India),pdf,DOCS/Digital Public Infrastructure (India).pdf
@@ -24,13 +21,11 @@ var data3Links = `ID,Categry,Subject,Remark,Link
 020,Ashma Pokharel -2079 batch,E-Residency Estonia,pdf,DOCS/E-Residency(Estonia).pdf
 021,Kritika Kathayat -2079 batch,e-police (1),pdf,DOCS/epolice (1).pdf
 022,Preeti Kumari Pandit-2079 batch,Global Development Lab (USA) 2,pdf,DOCS/Global Development Lab (USA) 2.pdf
-023,Jeeya Shakya-2078batch,Government Cloud Estonia,pdf,DOCS/govcloudbyjeeya.pdf
 024,Manisha Yadav-2079 batch,Government Cloud Estonia,powerpoint,DOCS/GOVERNMENT CLOUD ESTONIA.pptx
 025,Sapana Jha-2079 batch,Hyperloop Transportation,pdf,DOCS/Hyperloop Transportation.pdf
 026,Dipa Oli-2079 batch,KSI Blockchain Estonia,pdf,DOCS/KSI Blockchain Estonia final one.pdf
 027,Sunita Pariyar-2079batch,Smart Nation Singapore Summary,pdf,DOCS/Smart_Nation_Singapore_Summary.pdf
-028,Document,Civil Code 2074,word doc,DOCS/civilcode2074.docx
-029,Document,Land Related Act 2021,word doc,DOCS/landrelatedact2021.docx
+
 030,Aaryaka Timsina-2079 batch, e-identity by aaryaka, pdf doc, DOCS/e-identity aaryaka.pdf
 031,Mallika Dwariya -2079 batch, i-voting, pdf doc, DOCS/i-Voting (Estonia).pdf
 032,Astha Yadav-2079 batch,Smart ID Estonia by Astha Yadav,pdf,DOCS2/Smart ID Estonia by Astha Yadav.pdf
@@ -41,13 +36,17 @@ var data3Links = `ID,Categry,Subject,Remark,Link
 037,Anjali Chaudhary-2079 batch,X-Road Estonia by Anjali Chaudhary,pdf,DOCS2/X-Road Estonia by Anjali Chaudhary.pdf
 038,Merika Mote-2079 batch,Estonia by Merika Mote,pdf,DOCS2/Estonia by Merika Mote.pdf
 039,Dikshya-2078 batch,population registry of estonia,pdf,DOCS/populationregistryestonia.pdf
-040,Jeeya-2078 batch,government cloud of estonia,pdf,DOCS/govcloudbyjeeya.pdf
+
+023,Jeeya Shakya-2078batch,Government Cloud Estonia,pdf,DOCS/govcloudbyjeeya.pdf
 041,Divya-2078 batch,E-Waste recycling of China,pdf,DOCS/ewasterecyclingofchinabydivya.pdf
 042,Archana-2078 batch,X-Road of Estonia,pdf,DOCS/XroadEstoniabyarchana.pdf
 043,Pratikshya-2078 batch,Intelligent Transport System,pdf,DOCS/intelligenttransportbypratikshya.pdf
 044,Jamuna-2078 batch,e-police,pdf,DOCS/epolicebyjamuna.pdf
 045,Law,PPP Act 2075,pdf,DOCS/PPP Act 2075.pdf
 046,Policy,PPP Policy 2072,pdf,DOCS/PPP Policy 2072.pdf
+028,Document,Civil Code 2074,word doc,DOCS/civilcode2074.docx
+029,Document,Land Related Act 2021,word doc,DOCS/landrelatedact2021.docx
+001,Make a Call-2079batch,BPSG 7th sem-2082(Mansir),Contact Directory,https://shivaprasadacharya.github.io/callbpsgforthbatch/
 
 
 `;
